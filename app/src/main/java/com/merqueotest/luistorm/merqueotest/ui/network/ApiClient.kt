98a68111e7904f9b6437e496f25f452ccaf88228
luis <luistorresm0905@gmail.com>
2018-12-07 21:39:15 -0500

    Setup network things

---
+++ app/src/main/java/com/merqueotest/luistorm/merqueotest/ui/network/ApiClient.kt
@@ -0,0 +1,65 @@
+package com.merqueotest.luistorm.merqueotest.ui.network
+
+import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import com.merqueotest.luistorm.merqueotest.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class ApiClient {
+
+    companion object {
+
+        private var retrofit: Retrofit? = null
+        private const val REQUEST_TIMEOUT = 600
+        private var okHttpClient: OkHttpClient? = null
+
+        fun getClient(): Retrofit? {
+
+            if (okHttpClient == null)
+                initOkHttp()
+
+            if (retrofit == null) {
+                val gson = GsonBuilder()
+                    .setLenient()
+                    .create()
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BuildConfig.BASE_URL)
+                    .client(okHttpClient!!)
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build()
+            }
+            return retrofit
+        }
+
+        private fun initOkHttp() {
+            val httpClient = OkHttpClient().newBuilder()
+                .connectTimeout(REQUEST_TIMEOUT.toLong(), TimeUnit.SECONDS)
+                .readTimeout(REQUEST_TIMEOUT.toLong(), TimeUnit.SECONDS)
+                .writeTimeout(REQUEST_TIMEOUT.toLong(), TimeUnit.SECONDS)
+
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            httpClient.addInterceptor(interceptor)
+
+            httpClient.addInterceptor { chain ->
+                val original = chain.request()
+                val requestBuilder = original.newBuilder()
+                    .addHeader("Accept", "application/json")
+                    .addHeader("Content-Type", "application/json")
+
+                val request = requestBuilder.build()
+                chain.proceed(request)
+            }
+
+            okHttpClient = httpClient.build()
+        }
+
+    }
+
+}
