021a53a7cd5e948e01bea0bb9700b356449d9a65
luis <luistorresm0905@gmail.com>
2018-12-08 10:14:13 -0500

    Android lifecycle dependencies and MainViewModel var

--- app/src/main/java/com/merqueotest/luistorm/merqueotest/ui/activities/MainActivity.kt
+++ app/src/main/java/com/merqueotest/luistorm/merqueotest/ui/activities/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.merqueotest.luistorm.merqueotest.ui.activities
 
 import android.app.Dialog
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RelativeLayout
 import com.merqueotest.luistorm.merqueotest.R
+import com.merqueotest.luistorm.merqueotest.ui.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var dialog: Dialog
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 
     fun showLoader(context: Context) {
