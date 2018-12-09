d8fd9405a361b4b2e6739c3eeca6cfbcd1913a32
luis <luistorresm0905@gmail.com>
2018-12-09 09:49:20 -0500

    Added realm and a method to know if internet connection is available

--- app/src/main/java/com/merqueotest/luistorm/merqueotest/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/merqueotest/luistorm/merqueotest/ui/viewmodels/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.merqueotest.luistorm.merqueotest.ui.viewmodels
 import android.annotation.SuppressLint
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import android.content.Context
 import com.merqueotest.luistorm.merqueotest.BuildConfig
 import com.merqueotest.luistorm.merqueotest.ui.models.Movie
 import com.merqueotest.luistorm.merqueotest.ui.network.NetworkApis
@@ -10,6 +11,10 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.Subject
+import android.net.NetworkInfo
+import android.net.ConnectivityManager
+
+
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -22,6 +27,13 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
             .subscribe({
                 notifyPopularMoviesSubject.onNext(it.results)
             }, Throwable::printStackTrace)
+    }
+
+    fun hasInternetConnection(context: Context) : Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        return connectivityManager!!.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).state == NetworkInfo.State.CONNECTED || connectivityManager.getNetworkInfo(
+            ConnectivityManager.TYPE_WIFI
+        ).state == NetworkInfo.State.CONNECTED
     }
 
 }
